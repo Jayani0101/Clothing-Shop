ee40a3746200f26d2afc62749dc7b0bcb0b0a5b0
hpiyumi010-boop <hpiyumi010@gmail.com>
2026-04-13 16:31:54 +0530

    Serve media files in development

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include   # <-- add 'include'
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),     # <-- your UI and auth
     #path('shop/', include('shop.urls')), # <-- Member 1's products (optional, but keep)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
