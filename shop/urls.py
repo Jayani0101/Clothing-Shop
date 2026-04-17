dfe1adfcbdc80d3d5c1eb7f574e9293266e06db5
Jayani0101 <jayanilak07@gmail.com>
2026-04-17 18:09:38 +0530

    Refactor URL patterns for cart functionality
    Removed product-related URL patterns and added re_path for cart updates.

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-from django.urls import re_path
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('products/', views.product_list, name='products'),
-    path('category/<int:id>/', views.category_products, name='category_products'),
     path('cart/', views.cart, name='cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     re_path(r'^update-cart/(?P<product_id>\d+)/(?P<change>-?\d+)/$', views.update_cart, name='update_cart'),
@@ -13,5 +10,4 @@ urlpatterns = [
     path('place_order/', views.place_order, name='place_order'),
     path('orders/', views.order_history, name='order_history'),
     path('remove/<int:product_id>/', views.remove_from_cart, name='remove'),
-    path('product/<int:id>/', views.product_detail, name='product_detail'),
 ]
